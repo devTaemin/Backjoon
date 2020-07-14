516edcee5665b0cfdf643a9ca09cf38ac928681b
Taemin Im <devTaemin@gmail.com>
2020-07-15 01:34:47 +0900

    Update BaekJoon
    Date: 20200715
    
    - 002751 Sorting number2

---
+++ 002751_sorting-number2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int num;
+	int* arr;
+
+	cin >> num;
+	arr = new int[num];
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + num);
+
+	for (int j = 0; j < num; j++)
+	{
+		cout << arr[j] << '\n';
+	}
+}
